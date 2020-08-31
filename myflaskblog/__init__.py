bb0d62439d40857bce2f972b7a7e6b45594d4ed8
Muhammad Ahmed <muhammad.ahmed.azizi@gmail.com>
2020-08-31 14:32:15 +0500

    User Authentication Finalized - Part 6 Completed

--- myflaskblog/__init__.py
+++ myflaskblog/__init__.py
@@ -1,6 +1,10 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
 
 
 
@@ -11,5 +15,9 @@ app.config['SECRET_KEY'] = 'f18ba0230f5f7ccc16ca6ffa92f75997'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from myflaskblog import routes
