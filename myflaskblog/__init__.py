c80598b6010c3867c1ca19eeee4b4c3bfddc15a3
Muhammad Ahmed <muhammad.ahmed.azizi@gmail.com>
2020-09-05 18:23:15 +0500

    Email and Password Functionality Added

--- myflaskblog/__init__.py
+++ myflaskblog/__init__.py
@@ -1,8 +1,10 @@
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from myflaskblog.secret import email, ps
 
 
 
@@ -19,5 +21,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+#EMAIL SETTING FOR GMAIL
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['DEBUG'] = True
+app.config['MAIL_USERNAME'] = 'dummy@dummy.com'
+app.config['MAIL_PASSWORD'] = 'dummypassword'
+mail = Mail(app)
+
 
 from myflaskblog import routes
