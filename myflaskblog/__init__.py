3543b8d36c29f98209b4e29fab54800c3ea44af8
Muhammad Ahmed <muhammad.ahmed.azizi@gmail.com>
2020-09-08 13:17:54 +0500

    Flask Blueprints and Separte Configuration Added

--- myflaskblog/__init__.py
+++ myflaskblog/__init__.py
@@ -1,36 +1,42 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from myflaskblog.secret import email, ps
+from myflaskblog.config import Config
 
 
 
 
-
-app = Flask(__name__)
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'f18ba0230f5f7ccc16ca6ffa92f75997'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-#EMAIL SETTING FOR GMAIL
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-app.config['DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'dummy@dummy.com'
-app.config['MAIL_PASSWORD'] = 'dummypassword'
-mail = Mail(app)
 
 
-from myflaskblog import routes
+mail = Mail()
+
+
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from myflaskblog.users.routes import users
+    from myflaskblog.posts.routes import posts
+    from myflaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
+
---
+++ myflaskblog/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #SECRET_KEY = 'secretkey here - no own EV'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' now on EV
+    # EMAIL SETTING FOR GMAIL
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    DEBUG = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
